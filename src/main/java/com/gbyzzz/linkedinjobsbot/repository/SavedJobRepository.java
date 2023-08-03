c4909d0157ec9a53ec5ca66fbe1d356ecf6ad0a2
Anton Pinchuk <gbyzzz@gmail.com>
2023-08-04 01:07:47 +0300

    added docker

--- src/main/java/com/gbyzzz/linkedinjobsbot/repository/SavedJobRepository.java
+++ src/main/java/com/gbyzzz/linkedinjobsbot/repository/SavedJobRepository.java
@@ -9,7 +9,5 @@ import java.util.List;
 @Repository
 public interface SavedJobRepository extends JpaRepository<SavedJob, Long> {
     List<SavedJob> findSavedJobByUserProfile_ChatId(Long id);
-    List<SavedJob> findSavedJobByUserProfile_ChatIdAndAppliedEqualsOrDeletedEquals(Long id,
-                                                                                   boolean applied,
-                                                                                   boolean deleted);
+    List<SavedJob> findSavedJobByUserProfile_ChatIdAndReplyState(Long id, SavedJob.ReplyState state);
 }
