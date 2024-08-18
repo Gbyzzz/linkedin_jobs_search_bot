b6d9dde36765b1aed19b693f0aae7e9a77d43d19
Anton Pinchuk <gbyzzz@gmail.com>
2024-08-18 10:16:22 +0300

    add kafka SSL config

--- job-scanner/src/main/java/com/gbyzzz/jobscanner/config/KafkaConfig.java
+++ job-scanner/src/main/java/com/gbyzzz/jobscanner/config/KafkaConfig.java
@@ -21,71 +21,71 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableKafka
-@Configuration
+//@EnableKafka
+//@Configuration
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${application.kafka.topic.to_check_if_new}")
-    private String topicToCheckIfNew;
-
-    @Bean
-    public NewTopic toSearchTopic() {
-        return TopicBuilder.name(topicToCheckIfNew)
-                .partitions(1)
-                .replicas(1)
-                .build();
-    }
-
-    public Map<String, Object> kafkaConsumerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TYPE_MAPPINGS, "searchParamsTimeRangeDTO:com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
-//        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
-//        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.gbyzzz.telegramclient.dto");
-        return props;
-    }
-
-    @Bean
-    public ConsumerFactory<String, SearchParamsTimeRangeDTO> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(kafkaConsumerConfig(),
-                new StringDeserializer(), new JsonDeserializer<>(SearchParamsTimeRangeDTO.class, false));
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, SearchParamsTimeRangeDTO>>
-    kafkaListenerContainerFactory(ConsumerFactory<String, SearchParamsTimeRangeDTO> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, SearchParamsTimeRangeDTO> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        return factory;
-    }
-
-
-
-    public Map<String, Object> kafkaProducerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(JsonSerializer.TYPE_MAPPINGS,
-                        "searchParamsTimeRangeDTO:com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<String, SearchParamsTimeRangeDTO> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(kafkaProducerConfig());
-    }
-
-    @Bean
-    public KafkaTemplate<String, SearchParamsTimeRangeDTO> kafkaTemplate(
-            ProducerFactory<String, SearchParamsTimeRangeDTO> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
-    }
+//    @Value("${spring.kafka.bootstrap-servers}")
+//    private String bootstrapServers;
+//
+//    @Value("${application.kafka.topic.to_check_if_new}")
+//    private String topicToCheckIfNew;
+//
+//    @Bean
+//    public NewTopic toSearchTopic() {
+//        return TopicBuilder.name(topicToCheckIfNew)
+//                .partitions(1)
+//                .replicas(1)
+//                .build();
+//    }
+//
+//    public Map<String, Object> kafkaConsumerConfig() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        props.put(JsonDeserializer.TYPE_MAPPINGS, "searchParamsTimeRangeDTO:com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
+////        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
+////        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.gbyzzz.telegramclient.dto");
+//        return props;
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<String, SearchParamsTimeRangeDTO> consumerFactory() {
+//        return new DefaultKafkaConsumerFactory<>(kafkaConsumerConfig(),
+//                new StringDeserializer(), new JsonDeserializer<>(SearchParamsTimeRangeDTO.class, false));
+//    }
+//
+//    @Bean
+//    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, SearchParamsTimeRangeDTO>>
+//    kafkaListenerContainerFactory(ConsumerFactory<String, SearchParamsTimeRangeDTO> consumerFactory) {
+//        ConcurrentKafkaListenerContainerFactory<String, SearchParamsTimeRangeDTO> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory);
+//        return factory;
+//    }
+//
+//
+//
+//    public Map<String, Object> kafkaProducerConfig() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        props.put(JsonSerializer.TYPE_MAPPINGS,
+//                        "searchParamsTimeRangeDTO:com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, SearchParamsTimeRangeDTO> producerFactory() {
+//        return new DefaultKafkaProducerFactory<>(kafkaProducerConfig());
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, SearchParamsTimeRangeDTO> kafkaTemplate(
+//            ProducerFactory<String, SearchParamsTimeRangeDTO> producerFactory) {
+//        return new KafkaTemplate<>(producerFactory);
+//    }
 
 }
--- linkedin-jobs-bot-main/src/main/java/com/gbyzzz/linkedinjobsbot/config/KafkaConfig.java
+++ linkedin-jobs-bot-main/src/main/java/com/gbyzzz/linkedinjobsbot/config/KafkaConfig.java
@@ -20,73 +20,70 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-@EnableKafka
+//@Configuration
+//@EnableKafka
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+//    @Value("${spring.kafka.bootstrap-servers}")
+//    private String bootstrapServers;
 
-    @Value("${application.kafka.topic.to_search}")
-    private String topicToSearch;
+//    @Value("${application.kafka.topic.to_search}")
+//    private String topicToSearch;
+//
+//    @Bean
+//    public NewTopic toSearchTopic() {
+//        return TopicBuilder.name(topicToSearch)
+//                .partitions(2)
+//                .replicas(1)
+//                .build();
+//    }
 
-    @Bean
-    public NewTopic toSearchTopic() {
-        return TopicBuilder.name(topicToSearch)
-                .partitions(2)
-                .replicas(1)
-                .build();
-    }
-
-    public Map<String, Object> kafkaProducerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(JsonSerializer.TYPE_MAPPINGS,
-                        "searchParamsTimeRangeDTO:com.gbyzzz.linkedinjobsbot.dto.SearchParamsTimeRangeDTO");
-//        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(kafkaProducerConfig());
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
-    }
-
-
-
-
-    
-
-    public Map<String, Object> kafkaConsumerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-//        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.gbyzzz.jobscanner.dto.SearchParamsTimeRangeDTO");
-//        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.gbyzzz.telegramclient.dto");
-        props.put(JsonDeserializer.TYPE_MAPPINGS, "searchParamsTimeRangeDTO:com.gbyzzz.linkedinjobsbot.dto.SearchParamsTimeRangeDTO");
-        return props;
-    }
-
-    @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(kafkaConsumerConfig(),
-                new StringDeserializer(), new JsonDeserializer<>());
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>>
-    kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactoryToSave) {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactoryToSave);
-        return factory;
-    }
+//    public Map<String, Object> kafkaProducerConfig() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        props.put(JsonSerializer.TYPE_MAPPINGS,
+//                        "searchParamsTimeRangeDTO:com.gbyzzz.linkedinjobsbot.dto.SearchParamsTimeRangeDTO");
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, Object> producerFactory() {
+//        return new DefaultKafkaProducerFactory<>(kafkaProducerConfig());
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+//        return new KafkaTemplate<>(producerFactory);
+//    }
+//
+//
+//
+//
+//
+//
+//    public Map<String, Object> kafkaConsumerConfig() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        props.put(JsonDeserializer.TYPE_MAPPINGS, "searchParamsTimeRangeDTO:com.gbyzzz.linkedinjobsbot.dto.SearchParamsTimeRangeDTO");
+//        return props;
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<String, Object> consumerFactory() {
+//        return new DefaultKafkaConsumerFactory<>(kafkaConsumerConfig(),
+//                new StringDeserializer(), new JsonDeserializer<>());
+//    }
+//
+//    @Bean
+//    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>>
+//    kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactoryToSave) {
+//        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactoryToSave);
+//        return factory;
+//    }
 }
