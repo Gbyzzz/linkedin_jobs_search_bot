092fffb2b86ddc3314b24e26fcfeef0a99dbc680
Anton Pinchuk <gbyzzz@gmail.com>
2024-08-25 23:52:27 +0300

    some fixes

--- linkedin-jobs-bot-main/src/main/java/com/gbyzzz/linkedinjobsbot/service/scheduled/ScheduledService.java
+++ linkedin-jobs-bot-main/src/main/java/com/gbyzzz/linkedinjobsbot/service/scheduled/ScheduledService.java
@@ -1,28 +1,16 @@
 package com.gbyzzz.linkedinjobsbot.service.scheduled;
 
-import com.gbyzzz.linkedinjobsbot.controller.LinkedInJobsBot;
-import com.gbyzzz.linkedinjobsbot.controller.MessageText;
-import com.gbyzzz.linkedinjobsbot.controller.command.impl.ListOfJobsCommand;
 import com.gbyzzz.linkedinjobsbot.dto.mapper.SearchParamsTimeRangeDTOMapper;
 import com.gbyzzz.linkedinjobsbot.entity.SearchParams;
 import com.gbyzzz.linkedinjobsbot.entity.UserProfile;
-import com.gbyzzz.linkedinjobsbot.entity.converter.SendToEditMessageConverter;
-import com.gbyzzz.linkedinjobsbot.service.*;
+import com.gbyzzz.linkedinjobsbot.service.KafkaService;
+import com.gbyzzz.linkedinjobsbot.service.SearchParamsService;
+import com.gbyzzz.linkedinjobsbot.service.UserProfileService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.Chat;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.message.Message;
-import reactor.core.publisher.Flux;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -35,8 +23,8 @@ public class ScheduledService {
     private final SearchParamsTimeRangeDTOMapper searchParamsTimeRangeDTOMapper;
 
 
-    @Scheduled(cron = "0 0/1 * * * ?")
-    public void makeScan() throws IOException {
+    @Scheduled(cron = "0 0/30 * * * ?")
+    public void makeScan() {
         System.out.println("Scheduled");
         List<UserProfile> userProfiles = userProfileService.getAll();
         System.out.println(userProfiles);
